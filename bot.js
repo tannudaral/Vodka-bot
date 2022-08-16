7a795b54a540403263899a793323912187243b11
Tannu Daral <70472388+tannudaral@users.noreply.github.com>
2022-08-16 23:35:29 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,19 +12,31 @@ function readyDiscord() {
     console.log("💖");
 }
 
-const replies = ["🍾😩", "🥃🍻🍷🥂🍸🍾", "BICH BE SOBER 🍹", "NO, IT'S BEER TIME 🍺", "CHEERS 🍻", "🥃🥴🇷🇺", "VODKAAAHHHH!", "GIMME VODKA"];
+// "🍾😩", "🥃🍻🍷🥂🍸🍾", "BICH BE SOBER 🍹", "NO, IT'S BEER TIME 🍺", "CHEERS 🍻", "🥃🥴🇷🇺", "VODKAAAHHHH!", "GIMME VODKA", 
+const vodka_replies = ["https://tenor.com/XR2O.gif", "https://tenor.com/bikpd.gif", "https://tenor.com/bmwJ4.gif", "https://tenor.com/bbfwx.gif", "https://tenor.com/bz6Ot.gif", "https://tenor.com/bolzU.gif", "https://tenor.com/bmYtV.gif", "https://tenor.com/bsrtl.gif"];
+
+const no_uReplies = ["https://gfycat.com/alertdependablehorse", "https://tenor.com/bgOAI.gif", "https://tenor.com/5YeU.gif", "https://tenor.com/bLhUP.gif", "https://tenor.com/bzdr6.gif", "https://tenor.com/bRWR5.gif", "https://tenor.com/bR1Kb.gif", "https://tenor.com/7i4Q.gif"]
 
 client.on("message", gotMessage);
 
 async function gotMessage(msg) {
     let tokens = msg.content.split(" ");
-
+    if(tokens[0] === "!ping" || msg.content === "ping") {
+        msg.reply("pong");
+    }
+    
     // console.log(msg.content); 
     // msg.content === 'vodka' || msg.content === 'Vodka' || msg.content === 'VODKA' || msg.content === 'vOdka' || msg.content === 'VODKAAAH'
     if(tokens[0] === "!vodka" || msg.content === 'vodka' || msg.content === 'Vodka' || msg.content === 'VODKA' || msg.content === 'vOdka' || msg.content === 'VODKAAAH'){
         // msg.channel.send('GIMME VODKAAAAAAH');
-        const index = Math.floor(Math.random() * replies.length);
-        msg.channel.send(replies[index]);
+        const index = Math.floor(Math.random() * vodka_replies.length);
+        msg.channel.send(vodka_replies[index]);
+    }
+
+    if(msg.content === 'no u' || msg.content === 'No u' || msg.content === 'NO U' || msg.content === 'NO u' || msg.content === 'nO u' || msg.content === 'nO U' || msg.content === 'nO u'){
+        // msg.channel.send('GIMME VODKAAAAAAH');
+        const idx = Math.floor(Math.random() * no_uReplies.length);
+        msg.reply(no_uReplies[idx]);
     }
 
     else if (tokens[0] == "!gif") {
